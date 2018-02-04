6e95a5be6f1957299b0521d331fb1523cd7b378e
Lalbahadur786 <34732209+Lalbahadur786@users.noreply.github.com>
2018-02-04 23:57:08 +0530

    Add files via upload

---
+++ Tutorial No.3/Calculator_monolithic_scientific/src/calculator_logic/AlgebricOp.java
@@ -0,0 +1,10 @@
+package calculator_logic;
+
+public interface AlgebricOp {
+		public	double min(int arr[]);
+		public	double max(int arr[]);
+		public	double standarddev(int arr[]);
+		public	double var(int arr[]);
+		public 	double mean(int arr[]);
+		
+}
---
+++ Tutorial No.3/Calculator_monolithic_scientific/src/calculator_logic/GeometricOp.java
@@ -0,0 +1,8 @@
+package calculator_logic;
+
+public interface GeometricOp {
+	public double sin(double a);
+	public double cos(double a);
+	public double tan(double a);
+
+}
